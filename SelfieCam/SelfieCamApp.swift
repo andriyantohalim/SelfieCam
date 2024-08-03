6115f1cd0a9c5c0e258cc27824d5d293a756cfe7
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-03 17:56:52 +0800

    Initial Commit

---
+++ SelfieCam/SelfieCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfieCamApp.swift
+//  SelfieCam
+//
+//  Created by Andriyanto Halim on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SelfieCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
